cb9e760984ef20f9d03c9c28370e098f5e448fd5
c00kamunga <tsothalanti2@yahoo.com>
2020-08-31 14:57:24 -0700

    added helper method for getting schemes

---
+++ schemes/scheme-model.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
+
+
+module.exports = {
+    find,
+}
+
+
+function find() {
+    return db('schemes')
+}
